3d6e92c4ae63003143359e7bfa8b6920e99d3c01
zzinypark <zzinypark21@gamil.com>
2025-07-25 15:45:25 +0900

    ✨feat: chapter_03 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 from todo.views import todo_list, todo_info # 추가
+from users import views as user_view
 
 
 urlpatterns = [
 		path('admin/', admin.site.urls),
-    path('todo/', todo_list, name='todo_list'), # 추가
-    path('todo/<int:todo_id>/', todo_info, name='todo_info'), #추가
+        path('todo/', todo_list, name='todo_list'), # 추가
+        path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+        path('accounts/login/', user_view.user_login, name='user_login'),
+        path('accounts/signup/', user_view.user_signup, name='user_signup'),
+        path('accounts/', include('django.contrib.auth.urls')),
 ]
