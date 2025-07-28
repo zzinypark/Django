cc1e3f0fe2d84b5a65bda31bf22b0ba6d056cf6f
zzinypark <zzinypark21@gamil.com>
2025-07-28 15:50:33 +0900

    feat: chapter_04 과제 업로드

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from todo.views import todo_list, todo_info # 추가
+from todo.views import todo_list, todo_info, todo_create, todo_delete, todo_update
 from users import views as user_view
 
 
@@ -8,6 +8,11 @@ urlpatterns = [
 		path('admin/', admin.site.urls),
         path('todo/', todo_list, name='todo_list'), # 추가
         path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+        path('todo/create/', todo_create, name='todo_create'),
+        path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
+        path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
+
+        #user
         path('accounts/login/', user_view.user_login, name='user_login'),
         path('accounts/signup/', user_view.user_signup, name='user_signup'),
         path('accounts/', include('django.contrib.auth.urls')),
--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
+User = get_user_model()
+
 class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.TextField()
     start_date = models.DateField()
